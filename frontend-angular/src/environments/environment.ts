50045fb1d513e122839404412d0bc1fc725193f6
SebastianFerrando <se.ferrando@duocuc.cl>
2026-09-23 19:08:40 -0300

    Update environment.ts

--- frontend-angular/src/environments/environment.ts
+++ frontend-angular/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   ordersApiUrl: 'http://34.229.201.100:8080/api',
   catalogApiUrl: 'http://34.229.201.100:8081/api',
   azure: {
-    clientId: '205564ec-6b65-4749-9ef8-83b5efb16c63',
+    clientId: 'd383250f-c257-4aa9-8203-f395b0ea0ab2',
     tenantId: '76d549dd-1b81-4a0d-97d5-09040b6180c4',
     redirectUri: 'http://localhost:4200',
-    scope: 'api://205564ec-6b65-4749-9ef8-83b5efb16c63/access_as_user'
+    scope: 'api://17ee25a4-2f2f-44f0-b467-5133a1e81e24/access_as_user'
   }
 };
