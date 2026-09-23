46b29321f2bcb9803aba0c2bd9959ff5ad65dc4c
SebastianFerrando <se.ferrando@duocuc.cl>
2026-09-23 13:26:59 -0300

    Implementacion entra id

--- frontend-angular/src/environments/environment.development.ts
+++ frontend-angular/src/environments/environment.development.ts
@@ -5,9 +5,9 @@ export const environment = {
   ordersApiUrl: 'http://localhost:8080/api',
   catalogApiUrl: 'http://localhost:8081/api',
   azure: {
-    clientId: '205564ec-6b65-4749-9ef8-83b5efb16c63',
+    clientId: 'd383250f-c257-4aa9-8203-f395b0ea0ab2',
     tenantId: '76d549dd-1b81-4a0d-97d5-09040b6180c4',
     redirectUri: 'http://localhost:4200',
-    scope: 'api://205564ec-6b65-4749-9ef8-83b5efb16c63/access_as_user'
+    scope: 'api://17ee25a4-2f2f-44f0-b467-5133a1e81e24/access_as_user'
   }
 };
